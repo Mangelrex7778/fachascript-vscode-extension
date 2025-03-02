81c0c0bee6d1ddb4b4704611dcb6107d7e1b1798
Mangel Rex <Pibeemojistudios@gmail.com>
2025-03-02 13:04:46 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,10 @@
 const vscode = require('vscode');
-const fachascriptCommands = require('./fachascript_indentado.js');
-const fachascriptLinter = require('./fachascript_indentado_error.js');
-const fachascriptEjecutar = require('./fachascript_indentado_ejecutar.js');
+const fachascriptCommandsIndentado = require('./scripts/fachascript_indentado.js');
+const fachascriptCommandsBrackets = require('./scripts/fachascript_brackets.js');
+const fachascriptLinterIndentado = require('./scripts/fachascript_indentado_error.js');
+const fachascriptLinterBrackets = require('./scripts/fachascript_brackets_error.js');
+const fachascriptEjecutarIndentado = require('./scripts/fachascript_indentado_ejecutar.js');
+const fachascriptEjecutarBrackets = require('./scripts/fachascript_brackets_ejecutar.js');
 
 function activate(context) {
     console.log('¡La extensión FachaScript está activa!');
@@ -12,9 +15,14 @@ function activate(context) {
 
     // Función para actualizar los diagnósticos
     function updateDiagnostics(document) {
-        if (document && document.languageId === 'fachascript-indentado') {
-            const diagnostics = fachascriptLinter.analyzeDocument(document);
-            diagnosticCollection.set(document.uri, diagnostics);
+        if (document) {
+            if (document.languageId === 'fachascript-indentado') {
+                const diagnostics = fachascriptLinterIndentado.analyzeDocument(document);
+                diagnosticCollection.set(document.uri, diagnostics);
+            } else if (document.languageId === 'fachascript-brackets') {
+                const diagnostics = fachascriptLinterBrackets.analyzeDocument(document);
+                diagnosticCollection.set(document.uri, diagnostics);
+            }
         }
     }
 
@@ -34,16 +42,29 @@ function activate(context) {
         updateDiagnostics(vscode.window.activeTextEditor.document);
     }
 
-    // Comando para ejecutar el código
-    let executeCommand = vscode.commands.registerCommand('fachascript.ejecutar', () => {
-        if (vscode.window.activeTextEditor) {
+    // Comando para ejecutar FachaScript Indentado
+    let ejecutarIndentadoCommand = vscode.commands.registerCommand('fachascript.ejecutarIndentado', () => {
+        if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId === 'fachascript-indentado') {
             const document = vscode.window.activeTextEditor.document;
-            fachascriptEjecutar.executeDocument(document, vscode.window.createOutputChannel('FachaScript Ejecución'));
+            const outputChannel = vscode.window.createOutputChannel('FachaScript Indentado Ejecución');
+            fachascriptEjecutarIndentado.executeDocument(document, outputChannel);
         } else {
-            vscode.window.showInformationMessage('No hay un documento activo.');
+            vscode.window.showInformationMessage('No hay un documento activo o no es FachaScript Indentado.');
         }
     });
-    context.subscriptions.push(executeCommand);
+    context.subscriptions.push(ejecutarIndentadoCommand);
+
+    // Comando para ejecutar FachaScript Brackets
+    let ejecutarBracketsCommand = vscode.commands.registerCommand('fachascript.ejecutarBrackets', () => {
+        if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId === 'fachascript-brackets') {
+            const document = vscode.window.activeTextEditor.document;
+            const outputChannel = vscode.window.createOutputChannel('FachaScript Brackets Ejecución');
+            fachascriptEjecutarBrackets.executeDocument(document, outputChannel);
+        } else {
+            vscode.window.showInformationMessage('No hay un documento activo o no es FachaScript Brackets.');
+        }
+    });
+    context.subscriptions.push(ejecutarBracketsCommand);
 }
 
 function deactivate() { }
